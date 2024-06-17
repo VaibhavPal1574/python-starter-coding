27dfa09e7cf6087f4665e8c06e3870ca96100d8c
mydumbbells <mydumbbells@gmail.com>
2024-06-17 07:38:26 +0530

    adding loop questions

---
+++ For loop/Print_First_10_natural_numbers_using_while_loop.py
@@ -0,0 +1,19 @@
+# Exercise 1: Print First 10 natural numbers using while loop
+
+# Expected output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+range=0
+
+while range<10:
+    range = range+1
+    print(range)
